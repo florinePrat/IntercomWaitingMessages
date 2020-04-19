ab23600bdd6a0f107201d66f7099e32e9967d099
ToinonPolytech <toinon.georget@etu.umontpellier.fr>
2020-04-19 15:18:48 +0200

    sending text message with fetch OK - to be tested

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ if(!autoMessageContent){
 }
 autoMessageContent = document.getElementsByClassName("composer-inbox");
 if(lang==="FR"){
+    if($('#autoMessageContentFR').length){
+        $('#autoMessageContentFR').remove()
+    }
     $('.submenu__sections')[0].insertAdjacentHTML('beforeend','<div id="autoMessageContentFR" hidden="true"></div>');
     $('#autoMessageContentFR')[0].insertAdjacentHTML('beforeend',autoMessageContent[0].outerHTML);
     console.log("FR content message",$('#autoMessageContentFR'));
 }
 if(lang==="EN"){
+
+    if($('#autoMessageContentEN').length){
+        $('#autoMessageContentEN').remove()
+        console.log("previous message content removed")
+    }
     $('.submenu__sections')[0].insertAdjacentHTML('beforeend','<div id="autoMessageContentEN" hidden="true"></div>');
     $('#autoMessageContentEN')[0].insertAdjacentHTML('beforeend',autoMessageContent[0].outerHTML);
     console.log("EN content message",$('#autoMessageContentEN'));
--- pauseMode.js
+++ pauseMode.js
@@ -2,25 +2,42 @@ async function sendAutoMessageTo(conversation,alreadyContacted,autoMessageConten
     //   $('.conversation__card__header')[index].click();
     console.log("auto message function called")
     conversation.click();
+    const splittedURL = window.location.href.split("/");
+    const conversationId = splittedURL[splittedURL.length-1];
     await  sleep(2000);
-    console.log("auto message content get : ",autoMessageContent)
-    console.log($(".composer-inbox"))
+    const  new_token = Array.from(document.getElementsByTagName('meta')).find(m => m.name === 'csrf-token').content
+    await fetch("https://app.intercom.com/ember/conversation_parts", {
+        "headers": {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+            "content-type": "application/json; charset=UTF-8",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-client-height": "976",
+            "x-client-width": "1920",
+            "x-csrf-token": new_token,
+            "x-embercom-revision-id": "27636",
+            "x-embercom-route": "apps.app.inbox.inbox.conversations.conversation",
+            "x-requested-with": "XMLHttpRequest"
+        },
+        "referrer": "https://app.intercom.com/a/apps/nt4il1tw/inbox/inbox/all/conversations/"+conversationId+"",
+        "referrerPolicy": "no-referrer-when-downgrade",
+        "body": "{\"app_id\":\"nt4il1tw\",\"conversation_id\":\""+conversationId+"\",\"blocks\":" +
+            "[{\"type\":\"paragraph\",\"text\":\""+autoMessageContentFR[0].textContent+"\"}],\"admin_id\":\"3411736\",\"type\":\"comment\",\"sub_type\":\"comment\",\"lightweight_reply_type\":null,\"passive\":false,\"created_at\":\"2020-04-19T12:37:02.644Z\",\"text_direction\":\"ltr\",\"client_assigned_uuid\":\"fc949e80-eb3f-4e9e-a58a-8937f2352134\",\"admin_only\":false,\"taggings\":[],\"uploads\":[]" +
+            "}",
+        "method": "POST",
+        "mode": "cors",
+        "credentials": "include"
+    });
     await sleep(2000);
-    if(isFrench()){
-        $(".composer-inbox")[$(".composer-inbox").length-1].prepend(autoMessageContentFR[0]);
-    } else {
-        $(".composer-inbox")[$(".composer-inbox").length-1].prepend(autoMessageContentEN[0]);
-    }
-    await sleep(2000);
-    console.log("send button is : ",$(".o__primary.btn"))
-    $(".o__primary.btn")[0].click();
-    alreadyContacted.push(conversation);
+    alreadyContacted.push(conversation);s
     console.log("already contacted after sending a message",alreadyContacted)
     return alreadyContacted;
 }
 
 function isFrench() {
-    console.log("element : ",$($(".kv__pair")[1]).children().children().children())
+    console.log("lang : ",$($($(".kv__pair")[1]).children().children().children()[1]).attr("data-content"))
    let userLocation =  $($($(".kv__pair")[1]).children().children().children()[1]).attr("data-content");
    return userLocation.includes("France");
 }
@@ -41,9 +58,9 @@ async function main(){
     let autoMessageContentFR=$('#autoMessageContentFR').children().children();
     let autoMessageContentEN=$('#autoMessageContentEN').children().children();
     while(paused && j<50){
+
         console.log("iteration n°",j);
         conversations = document.getElementsByClassName("conversation__card__header");
-        console.log(isFrench());
         //await sendAutoMessageTo(conversations[1]);
         if(!previousLastConversation){
             previousLastConversation = conversations[conversations.length-1];
@@ -53,7 +70,7 @@ async function main(){
         {
             console.log("verif premiere conv");
             i=conversations.length-1;
-            while(previousLastConversation==conversations[i]){
+            while(previousLastConversation!==conversations[i]){
                 console.log("new message detected ");
                 if(alreadyContacted && !alreadyContacted.includes(conversations[i])) {
                     console.log("already contacted : ",alreadyContacted);
